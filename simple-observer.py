e1d7e7f041cfc3a2819eb2dd328f967673343198
Adrian Lizarraga <adrian.lizarraga@zybooks.com>
2017-06-02 17:06:17 -0700

    Created custom class for converting to JSON

--- simple-observer.py
+++ simple-observer.py
@@ -5,10 +5,71 @@ from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 from watchdog.events import FileSystemEventHandler
 from lxml import etree
-
-from xmljson import badgerfish as bf
+from collections import Counter
 
 import json
+
+from xmljson import XMLData
+
+class ZyBooksConvention(XMLData):
+    '''Converts between XML and data using a modification of the BadgerFish convention'''
+
+    def __init__(self, **kwargs):
+        super(ZyBooksConvention, self).__init__(attr_prefix='@', text_content='$', **kwargs)
+
+    def data(self, root):
+        '''Convert etree.Element into a dictionary'''
+
+        value = self.dict()
+        children = [node for node in root if isinstance(node.tag, basestring)]
+        for attr, attrval in root.attrib.items():
+            attr = attr if self.attr_prefix is None else self.attr_prefix + attr
+            value[attr] = self._fromstring(attrval)
+
+        # Check for mixed content
+        has_children = len(children) > 0
+        root_has_text = root.text is not None and root.text.strip()
+        child_has_tail = any(child.tail is not None and child.tail.strip() for child in children)
+        has_mixed_content = has_children and (root_has_text or child_has_tail)
+        mixed_content_key = '#'
+
+        # If the element doesn't have mixed content, just execute the BadgerFish convention
+        if not has_mixed_content:
+            if root.text and self.text_content is not None:
+                text = root.text.strip()
+                if text:
+                    if self.simple_text and len(children) == len(root.attrib) == 0:
+                        value = self._fromstring(text)
+                    else:
+                        value[self.text_content] = self._fromstring(text)
+            count = Counter(child.tag for child in children)
+            for child in children:
+                if count[child.tag] == 1:
+                    value.update(self.data(child))
+                else:
+                    result = value.setdefault(child.tag, self.list())
+                    result += self.data(child).values()
+
+        # Otherwise, if the element has mixed content, add individual text content and children to a list
+        else:
+            value[mixed_content_key] = list()
+            root_text = root.text.strip() if root.text is not None else None
+
+            # Append root text
+            if root_text:
+                value[mixed_content_key].append({self.text_content: root_text})
+
+            # Append children and tails
+            for child in children:
+                value[mixed_content_key].append(self.data(child))
+
+                child_tail = child.tail.strip() if child.tail is not None else None
+                if child_tail:
+                    value[mixed_content_key].append({self.text_content: child_tail})
+
+        return self.dict([(root.tag, value)])
+
+xmljson_convention = ZyBooksConvention()
 
 class XMLObserver(FileSystemEventHandler):
 
@@ -35,7 +96,7 @@ class XMLObserver(FileSystemEventHandler):
         try:
             root = etree.fromstring(sample_section, parser)
 
-            print json.dumps(bf.data(root))
+            print json.dumps(xmljson_convention.data(root))
         except etree.XMLSyntaxError, e:
             print 'ERROR'
 
